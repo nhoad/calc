afbfd6d36f863f5642ddc9c07e71e43cbc93ff47
Nathan Hoad <nathan@getoffmalawn.com>
2015-09-26 23:27:00 +1000

    Remove the majority of globals.

--- calc.py
+++ calc.py
@@ -35,16 +35,179 @@ EDIT_Y = 0
 COLUMN_OFFSET = 1
 COLUMN_WIDTH = 10
 
-HIDE_OVERFLOW = True
-YANK = None
-
-FILENAME = None
+SPREADSHEET = None
 
 
-def debug(s):
-    with open('debug', 'a') as f:
-        f.write(str(s))
-        f.write('\n')
+class SpreadSheet:
+    cells = {}
+    filename = None
+
+    hide_overflow = True
+    column_width = 8
+
+    position = Point(0, 0)
+    sheet_pos = Point(0, 0)
+
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+        self.sheet = curses.newpad(MAX_ROWS, (MAX_COLUMNS + 1) * COLUMN_WIDTH)  # A-Z plus column for descriptions
+
+    def handle_input(self, ch):
+        if ch in (curses.KEY_DOWN, ord('j')):
+            self.chgat(curses.A_NORMAL)
+            self.position = self.position._replace(y=min(self.position.y+1, MAX_ROWS - ROW_START - 1))
+        elif ch in (curses.KEY_UP, ord('k')):
+            self.chgat(curses.A_NORMAL)
+            self.position = self.position._replace(y=max(self.position.y-1, 0))
+        elif ch in (curses.KEY_LEFT, ord('h')):
+            self.chgat(curses.A_NORMAL)
+            self.position = self.position._replace(x=max(self.position.x-1, 0))
+        elif ch in (curses.KEY_RIGHT, ord('l')):
+            self.chgat(curses.A_NORMAL)
+            self.position = self.position._replace(x=min(self.position.x+1, MAX_COLUMNS-1))
+        elif ch == ord('q'):
+            sys.exit(0)
+        elif ch == ord('n'):
+            self.cells.clear()
+        elif ch == ord('-'):
+            self.column_width -= 1
+        elif ch == ord('+'):
+            self.column_width += 1
+        elif ch == ord('='):
+            self.column_width = 8
+        elif ch == ord('H'):
+            self.hide_overflow = not self.hide_overflow
+        elif ch in (ord('y'), ord('d')):
+            cell = self.cells.get(self.position, None)
+            if cell is not None:
+                self.yank = cell.text
+            if ch == ord('d'):
+                self.cells.pop(self.position, None)
+        elif ch == ord('p'):
+            if self.yank is not None:
+                self.cells[self.position] = Cell(self.yank)
+        elif ch == ord('w'):
+            s = self.prompt("SAVE TO:", default=self.filename)
+            if s:
+                self.save(s)
+        elif ch in (curses.KEY_ENTER, 10, ord('i')):
+            cell = self.cells.get(self.position, None)
+
+            if cell is None:
+                default = ''
+            else:
+                default = cell.text
+
+            s = self.prompt("EDIT:", default=default)
+            if not s:
+                self.cells.pop(self.position, None)
+            else:
+                self.cells[self.position] = Cell(s)
+
+    def save(self, filename):
+        self.filename = filename
+
+        expanded_cells = dict(self.cells)
+
+        for position in self.cells:
+            # pad out any empty rows
+            for y in range(position.y+1):
+                new_pos = Point(0, y)
+                if new_pos not in expanded_cells:
+                    expanded_cells[new_pos] = Cell('')
+
+            # pad out any empty columns
+            for x in range(position.x+1):
+                new_pos = position._replace(x=x)
+                if new_pos not in expanded_cells:
+                    expanded_cells[new_pos] = Cell('')
+
+        rows = {}
+        for position, cell in expanded_cells.items():
+            rows.setdefault(position.y, {})[position.x] = cell.text
+
+        with open(filename, 'w') as f:
+            writer = csv.writer(f)
+
+            for y, row in sorted(rows.items()):
+                writer.writerow([v for (k, v) in sorted(row.items())])
+
+    def load(self, filename):
+        self.filename = filename
+
+        with open(filename, 'rU') as f:
+            reader = csv.reader(f)
+
+            for y, fields in enumerate(reader):
+                for x, field in enumerate(fields):
+                    if not field:
+                        continue
+                    position = Point(x, y)
+                    self.cells[position] = Cell(field)
+
+    def prompt(self, text, default=''):
+        def handle_key(key):
+            if key == 127:  # convert backspace into CTRL-H
+                return ord(curses.ascii.ctrl('h'))
+            return key
+
+        def maketextbox(h, w, y, x, value=""):
+            nw = curses.newwin(h, w, y, x)
+            txtbox = curses.textpad.Textbox(nw, insert_mode=True)
+
+            nw.addstr(0, 0, value)
+
+            self.stdscr.refresh()
+            self.stdscr.keypad(1)
+            try:
+                return txtbox.edit(handle_key).strip()
+            except KeyboardInterrupt:
+                return None
+            finally:
+                self.stdscr.keypad(0)
+
+        self.redraw()
+        self.stdscr.addstr(0, 0, text)
+        self.stdscr.refresh()
+        input = maketextbox(1, 40, EDIT_Y, EDIT_X, default)
+        return input
+
+    def draw_headings(self):
+        char = 'A'
+
+        for i in range(MAX_COLUMNS):
+            self.sheet.addstr(0, (i + COLUMN_OFFSET) * COLUMN_WIDTH, char.center(COLUMN_WIDTH))
+            char = chr(ord(char) + 1)
+
+        for i, y in enumerate(range(1, MAX_ROWS), 1):
+            self.sheet.addstr(y, COLUMN_OFFSET, str(i))
+
+    def draw_data(self):
+        for position, cell in self.cells.items():
+            adjusted_y = position.y + ROW_START
+            adjusted_x = (position.x + COLUMN_OFFSET) * COLUMN_WIDTH
+            self.sheet.addstr(adjusted_y, adjusted_x, cell.render().rjust(COLUMN_WIDTH))
+
+    def chgat(self, opts):
+        adjusted_y = self.position.y + ROW_START
+        adjusted_x = (self.position.x + COLUMN_OFFSET) * COLUMN_WIDTH - COLUMN_OFFSET
+        self.sheet.chgat(adjusted_y, adjusted_x, COLUMN_WIDTH+1, opts)
+
+    def redraw(self):
+        self.stdscr.clear()
+        self.sheet.clear()
+        self.draw_headings()
+        self.draw_data()
+        self.chgat(curses.A_REVERSE)  # highlight current cell
+        try:
+            self.stdscr.addstr(EDIT_Y, EDIT_X, self.cells[self.position].text)
+        except Exception:
+            pass
+        self.stdscr.refresh()
+
+        max_y, max_x = self.stdscr.getmaxyx()
+
+        self.sheet.refresh(self.sheet_pos.y, self.sheet_pos.x, ROW_START, COLUMN_OFFSET, max_y-ROW_START, max_x-COLUMN_OFFSET)
 
 
 class Cell(namedtuple('Cell', 'text')):
@@ -60,7 +223,7 @@ class Cell(namedtuple('Cell', 'text')):
             except Exception:
                 pass
 
-        if HIDE_OVERFLOW:
+        if SPREADSHEET.hide_overflow:
             return self.text[:COLUMN_WIDTH-1]
         return self.text
 
@@ -76,21 +239,21 @@ class Cell(namedtuple('Cell', 'text')):
                 ord(col_start.upper()) - ord('A'),
                 int(row_start) - 1,
             )
-            if p in cells:
-                _cells.append(cells[p])
+            if p in SPREADSHEET.cells:
+                _cells.append(SPREADSHEET.cells[p])
 
             p = Point(
                 ord(col_end.upper()) - ord('A'),
                 int(row_end) - 1,
             )
-            if p in cells:
-                _cells.append(cells[p])
+            if p in SPREADSHEET.cells:
+                _cells.append(SPREADSHEET.cells[p])
         else:
             for y in range(int(row_start)-1, int(row_end)):
                 for x in range(ord(col_start.upper()) - ord('A'), ord(col_end.upper()) - ord('A') + 1):
                     p = Point(x, y)
-                    if p in cells:
-                        _cells.append(cells[p])
+                    if p in SPREADSHEET.cells:
+                        _cells.append(SPREADSHEET.cells[p])
 
         return getattr(self, func)(_cells)
 
@@ -112,183 +275,24 @@ class Cell(namedtuple('Cell', 'text')):
         return '%.2f' % functools.reduce(operator.mul, (float(c.render()) for c in cells))
 
 
-cells = {}
-
-
 def main(stdscr):
+    global SPREADSHEET
     assert MAX_COLUMNS <= 26
 
     stdscr.idlok(True)
     stdscr.scrollok(True)
 
+    SPREADSHEET = SpreadSheet(stdscr)
+
     if len(sys.argv) > 1:
-        path = sys.argv[1]
-        global FILENAME
-        FILENAME = path
-
-        with open(path, 'rU') as f:
-            reader = csv.reader(f)
-
-            for y, fields in enumerate(reader):
-                for x, field in enumerate(fields):
-                    if not field:
-                        continue
-                    position = Point(x, y)
-                    cells[position] = Cell(field)
-
-    sheet = curses.newpad(MAX_ROWS, (MAX_COLUMNS + 1) * COLUMN_WIDTH)  # A-Z plus column for descriptions
-
-    def prompt(text, default=''):
-        def handle_key(key):
-            if key == 127:  # convert backspace into CTRL-H
-                return ord(curses.ascii.ctrl('h'))
-            return key
-
-        def maketextbox(h, w, y, x, value=""):
-            nw = curses.newwin(h, w, y, x)
-            txtbox = curses.textpad.Textbox(nw, insert_mode=True)
-
-            nw.addstr(0, 0, value)
-
-            stdscr.refresh()
-            stdscr.keypad(1)
-            try:
-                return txtbox.edit(handle_key).strip()
-            except KeyboardInterrupt:
-                return None
-            finally:
-                stdscr.keypad(0)
-
-        redraw()
-        stdscr.addstr(0, 0, text)
-        stdscr.refresh()
-        input = maketextbox(1, 40, EDIT_Y, EDIT_X, default)
-        return input
-
-    def draw_headings():
-        char = 'A'
-
-        for i in range(MAX_COLUMNS):
-            sheet.addstr(0, (i + COLUMN_OFFSET) * COLUMN_WIDTH, char.center(COLUMN_WIDTH))
-            char = chr(ord(char) + 1)
-
-        for i, y in enumerate(range(1, MAX_ROWS), 1):
-            sheet.addstr(y, COLUMN_OFFSET, str(i))
-
-    def draw_data():
-        for position, cell in cells.items():
-            adjusted_y = position.y + ROW_START
-            adjusted_x = (position.x + COLUMN_OFFSET) * COLUMN_WIDTH
-            sheet.addstr(adjusted_y, adjusted_x, cell.render().rjust(COLUMN_WIDTH))
-
-    def chgat(opts):
-        adjusted_y = position.y + ROW_START
-        adjusted_x = (position.x + COLUMN_OFFSET) * COLUMN_WIDTH - COLUMN_OFFSET
-        sheet.chgat(adjusted_y, adjusted_x, COLUMN_WIDTH+1, opts)
-
-    def redraw():
-        stdscr.clear()
-        sheet.clear()
-        draw_headings()
-        draw_data()
-        chgat(curses.A_REVERSE)  # highlight current cell
-        try:
-            stdscr.addstr(EDIT_Y, EDIT_X, cells[position].text)
-        except Exception:
-            pass
-        stdscr.refresh()
-        sheet.refresh(sheet_pos.y, sheet_pos.x, ROW_START, COLUMN_OFFSET, max_y-ROW_START, max_x-COLUMN_OFFSET)
-
-    position = Point(0, 0)
-    sheet_pos = Point(0, 0)
+        SPREADSHEET.load(sys.argv[1])
 
     while True:
-        max_y, max_x = stdscr.getmaxyx()
-
-        redraw()
+        SPREADSHEET.redraw()
 
         ch = stdscr.getch()
 
-        if ch in (curses.KEY_DOWN, ord('j')):
-            chgat(curses.A_NORMAL)
-            position = position._replace(y=min(position.y+1, MAX_ROWS - ROW_START - 1))
-        elif ch in (curses.KEY_UP, ord('k')):
-            chgat(curses.A_NORMAL)
-            position = position._replace(y=max(position.y-1, 0))
-        elif ch in (curses.KEY_LEFT, ord('h')):
-            chgat(curses.A_NORMAL)
-            position = position._replace(x=max(position.x-1, 0))
-        elif ch in (curses.KEY_RIGHT, ord('l')):
-            chgat(curses.A_NORMAL)
-            position = position._replace(x=min(position.x+1, MAX_COLUMNS-1))
-        elif ch == ord('q'):
-            break
-        elif ch == ord('n'):
-            cells.clear()
-        elif ch == ord('-'):
-            global COLUMN_WIDTH
-            COLUMN_WIDTH -= 1
-        elif ch == ord('+'):
-            global COLUMN_WIDTH
-            COLUMN_WIDTH += 1
-        elif ch == ord('='):
-            global COLUMN_WIDTH
-            COLUMN_WIDTH = 8
-        elif ch == ord('H'):
-            global HIDE_OVERFLOW
-            HIDE_OVERFLOW = not HIDE_OVERFLOW
-        elif ch in (ord('y'), ord('d')):
-            global YANK
-            cell = cells.get(position, None)
-            if cell is not None:
-                YANK = cell.text
-            if ch == ord('d'):
-                cells.pop(position, None)
-        elif ch == ord('p'):
-            if YANK is not None:
-                cells[position] = Cell(YANK)
-        elif ch == ord('w'):
-            s = prompt("SAVE TO:", default=FILENAME)
-            if s:
-                global FILENAME
-                FILENAME = s
-                expanded_cells = dict(cells)
-
-                for position in cells:
-                    # pad out any empty rows
-                    for y in range(position.y+1):
-                        new_pos = Point(0, y)
-                        if new_pos not in expanded_cells:
-                            expanded_cells[new_pos] = Cell('')
-
-                    # pad out any empty columns
-                    for x in range(position.x+1):
-                        new_pos = position._replace(x=x)
-                        if new_pos not in expanded_cells:
-                            expanded_cells[new_pos] = Cell('')
-
-                rows = {}
-                for position, cell in expanded_cells.items():
-                    rows.setdefault(position.y, {})[position.x] = cell.text
-
-                with open(s, 'w') as f:
-                    writer = csv.writer(f)
-
-                    for y, row in sorted(rows.items()):
-                        writer.writerow([v for (k, v) in sorted(row.items())])
-        elif ch in (curses.KEY_ENTER, 10, ord('i')):
-            cell = cells.get(position, None)
-
-            if cell is None:
-                default = ''
-            else:
-                default = cell.text
-
-            s = prompt("EDIT:", default=default)
-            if not s:
-                cells.pop(position, None)
-            else:
-                cells[position] = Cell(s)
+        SPREADSHEET.handle_input(ch)
 
 
 if __name__ == '__main__':
